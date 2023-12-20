6bdea258299655a570622519171e7881cb5550dc
B4night <superchaovaction@163.com>
2023-12-20 09:44:49 +0800

    add 2706.cpp
    Signed-off-by: B4night <superchaovaction@163.com>

---
+++ 2706.cpp
@@ -0,0 +1,13 @@
+#include "header.h"
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        return money >= prices[0] + prices[1] ? money - prices[0] - prices[1] : money;
+    }
+};
+
+int main() {
+
+}
