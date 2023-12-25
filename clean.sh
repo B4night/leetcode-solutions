83e29ae0f57bad4825b95d1133e82ca49cf78b07
B4night <superchaovaction@163.com>
2023-12-25 16:21:15 +0800

    bugfix: clean.sh
    Signed-off-by: B4night <superchaovaction@163.com>

--- clean.sh
+++ clean.sh
@@ -1,2 +1,3 @@
 # this shell script is used for clean executable files, which is files whose names are made up with numbers.
-find . -type f -name '[0-9]*' -exec rm -f {} +
+ls | grep -E "^[0-9]*$" | xargs -I {} rm -rf {}
+
